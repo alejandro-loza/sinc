7c890aab9eb14dc4bc26647c17e357865cbcbb48
Alejandro García Granados <alejandro.garcia@dinerio.mx>
2018-02-01 17:35:40 -0600

    Initial commit

---
+++ src/main/groovy/mx/finerio/synchronizer/SynchronizerApplication.groovy
@@ -0,0 +1,12 @@
+package mx.finerio.synchronizer
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SynchronizerApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run SynchronizerApplication, args
+	}
+}
