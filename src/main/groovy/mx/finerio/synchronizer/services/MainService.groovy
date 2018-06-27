cff1b5bda20e17af463d6bc635708da5eab9ac38
Alejandro García Granados <alejandro.garcia@dinerio.mx>
2018-06-27 15:58:24 -0500

    Modified cron expressions in order to run every Wednesday

--- src/main/groovy/mx/finerio/synchronizer/services/MainService.groovy
+++ src/main/groovy/mx/finerio/synchronizer/services/MainService.groovy
@@ -29,42 +29,42 @@ class MainService {
   @Autowired
   FinancialInstitutionRepository financialInstitutionRepository
 
-  @Scheduled(cron = '0 0 3 * * *')
+  @Scheduled(cron = '0 0 3 * * 3')
   void runBanamex() {
     runByInstitutionId( 2L )
   }
 
-  @Scheduled(cron = '0 0 2 * * *')
+  @Scheduled(cron = '0 0 2 * * 3')
   void runSantander() {
     runByInstitutionId( 7L )
   }
 
-  @Scheduled(cron = '0 1 2 * * *')
+  @Scheduled(cron = '0 1 2 * * 3')
   void runHsbc() {
     runByInstitutionId( 8L )
   }
 
-  @Scheduled(cron = '0 2 2 * * *')
+  @Scheduled(cron = '0 2 2 * * 3')
   void runAmex() {
     runByInstitutionId( 9L )
   }
 
-  @Scheduled(cron = '0 3 2 * * *')
+  @Scheduled(cron = '0 3 2 * * 3')
   void runInvex() {
     runByInstitutionId( 10L )
   }
 
-  @Scheduled(cron = '0 4 2 * * *')
+  @Scheduled(cron = '0 4 2 * * 3')
   void runScotiabank() {
     runByInstitutionId( 11L )
   }
 
-  @Scheduled(cron = '0 0 9 * * *')
+  @Scheduled(cron = '0 0 9 * * 3')
   void runBanorte() {
     runByInstitutionId( 12L )
   }
 
-  @Scheduled(cron = '0 5 2 * * *')
+  @Scheduled(cron = '0 5 2 * * 3')
   void runInbursa() {
     runByInstitutionId( 13L )
   }
